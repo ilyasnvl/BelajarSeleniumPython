fe66991088b12c6dde6aeaf22fe4518ad7d3d16b
ilyasnvl <58258897+ilyasnvl@users.noreply.github.com>
2023-05-21 21:33:12 +0700

    Add files via upload

---
+++ logintokped.py
@@ -0,0 +1,26 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.tokopedia.com/")
+
+def test_login():
+    try:
+        driver.find_element(By.CSS_SELECTOR, "[data-testid=btnHeaderLogin]").click()
+        WebDriverWait(driver,10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "css-1f0x5lg-unf-modal.e1nc1fa21")))
+        driver.find_element(By.ID, "email-phone").send_keys("testing@tokopedia.com")
+        driver.find_element(By.ID, "email-phone-submit").click()
+        WebDriverWait(driver,10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "css-1f0x5lg-unf-modal.e1nc1fa21")))
+        driver.find_element(By.CLASS_NAME, "css-ufaufa erj7th73").send_keys("tokopedia789")
+        #driver.find_element(By.CSS_SELECTOR, "span[aria-label='login-button']").click()
+        time.sleep(10)
+    except TimeoutException:
+        pass    
+    
+    
