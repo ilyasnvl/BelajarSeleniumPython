fa070adfa33b09a8c8c828f2cd58dcac9895926f
ilyasnvl <ilyasnauval96@gmail.com>
2023-05-26 17:51:34 +0700

    add tes case logout

--- logintokped.py
+++ logintokped.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 driver = webdriver.Chrome()
@@ -15,13 +16,18 @@ def test_login():
         WebDriverWait(driver,10).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "css-1f0x5lg-unf-modal.e1nc1fa21")))
         driver.find_element(By.ID, "email-phone").send_keys("pbs-ilyas+pmb1@tokopedia.com")
         driver.find_element(By.ID, "email-phone-submit").click()
-        time.sleep(5)
+        time.sleep(2)
         driver.find_element(By.ID, "password-input").send_keys("tokopedia789")
         driver.find_element(By.CSS_SELECTOR, "span[aria-label='login-button']").click()
         time.sleep(5)
 
 def test_logout():
-        driver.find_element()
-     
-    
+        # menu = driver.find_element(By.ID, "my-profile-header")
+        # hover = ActionChains(driver).move_to_element(menu)
+        # hover.perform()
+
+        ActionChains(driver).move_to_element((driver.find_element(By.ID, "my-profile-header"))).perform()
+        ActionChains(driver).move_to_element((driver.find_element(By.CSS_SELECTOR, ".css-1juts7j"))).click
+        driver.find_element(By.CLASS_NAME, "css-1juts7j").click
+        time.sleep(50)
     
